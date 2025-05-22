6eeea828d8b1bebe859b23aa7da652abcaacb5fd
LazyMG <cbfmark@gmail.com>
2025-05-22 15:41:57 +0900

    style: UI 수정 반영

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,119 +1,102 @@
 export default function BreadPage() {
   return (
-    <div className="w-full min-h-screen flex flex-col box-border">
-      {/* <header className="w-full h-[90px] shadow-md bg-white"></header> */}
-      <div
-        id="main"
-        className="px-[17px] flex flex-col flex-1"
-        style={{
-          background: `linear-gradient(
-      to bottom,
-      rgba(227, 240, 255, 0.2) 8%,
-      rgba(227, 240, 255, 0) 84%
-    )`,
-        }}
-      >
-        <div className="w-full min-h-[333px] border-b-[1px] border-b-black">
-          <div className="pt-[35px] ml-[7px] font-bold text-[20px] font-localWanted">
-            오늘의 양식
-          </div>
-          <div className="pt-[18px] ml-[10px] flex items-start">
-            <div className="h-[78px] font-bold text-[64px] font-alte flex items-center">
-              30
-            </div>
-            <div className="h-[24px] font-bold text-[20px] pt-[13px] pl-[13px] font-alte">
+    <div className="w-full flex flex-col box-border bg-white">
+      <div id="main" className="flex flex-col flex-1">
+        <div className="w-full h-[279px] pl-[27px]">
+          <div className="mt-[19px] ml-[1px] flex items-start">
+            <span className="font-alte font-bold text-[64px] h-[78px] flex items-center">
+              05/30
+            </span>
+            <span className="font-alte font-bold text-[20px] pt-[13px] pl-[9px]">
               Tue
-            </div>
+            </span>
           </div>
-          <div className="pt-[4px] ml-[9px] flex items-start pr-[16px]">
-            <div className="w-[232px] min-h-[72px] text-[28px] leading-[36px] font-bold font-wanted break-words">
+          <div className="mt-[4px] flex items-start pr-[31px]">
+            <div className="font-wanted font-bold text-[24px] leading-[32px] w-[199px] break-words">
               야곱의 노력과 승리는 어디로부터 나오나
             </div>
-            <div className="ml-auto h-[32px] pt-[6px] text-[12px] leading-[16px] font-wanted text-left flex-shrink-0">
+            <div className="ml-auto mt-[6px] text-[12px] leading-[16px] font-wanted text-left h-[32px] flex-shrink-0">
               <div>창세기</div>
               <div>30 : 25-43</div>
             </div>
           </div>
         </div>
-        <div className="w-full min-h-[155px] border-b-[1px] border-b-black">
-          <div className="pt-[22px] ml-[7px] font-bold text-[20px] font-wanted leading-[22px]">
+        <div className="w-full h-[156px] bg-[#CBECFF] pl-[24px]">
+          <div className="mt-[22px] font-wanted font-bold text-[16px] leading-[22px]">
             일주일의 기록
           </div>
-          <div className="w-full flex pl-[16px] pr-[16px] mt-[24px] justify-between">
-            <div className="flex flex-col gap-[3px] items-center">
-              <div className="w-[36px] h-[36px] rounded-full bg-[#E3F0FF] border-[#333333] border text-[13px] font-alte font-bold flex justify-center items-center">
+          <div className="mt-[18px] pl-[9px] pr-[33px] flex justify-between">
+            <div className="flex flex-col gap-[5px] items-center">
+              <div className="font-alte font-bold text-[11px] text-[rgba(51,51,51,0.5)]">
+                SUN
+              </div>
+              <div className="w-[36px] h-[36px] rounded-full bg-white text-[13px] font-alte font-bold flex justify-center items-center text-[rgba(51,51,51,0.5)]">
                 11
               </div>
-              <div className="text-[14px] font-wanted leading-[24px]">일</div>
             </div>
-            <div className="flex flex-col gap-[3px] items-center">
-              <div className="w-[36px] h-[36px] rounded-full bg-[#E3F0FF] border-[#333333] border text-[13px] font-alte font-bold flex justify-center items-center">
+            <div className="flex flex-col gap-[5px] items-center">
+              <div className="font-alte font-bold text-[11px] text-[rgba(51,51,51,0.5)]">
+                MON
+              </div>
+              <div className="w-[36px] h-[36px] rounded-full bg-white text-[13px] font-alte font-bold flex justify-center items-center text-[rgba(51,51,51,0.5)]">
                 12
               </div>
-              <div className="text-[14px] font-wanted leading-[24px]">월</div>
             </div>
-            <div className="flex flex-col gap-[3px] items-center">
-              <div className="w-[36px] h-[36px] rounded-full bg-[#E3F0FF] border-[#333333] border text-[13px] font-alte font-bold flex justify-center items-center">
+            <div className="flex flex-col gap-[5px] items-center">
+              <div className="font-alte font-bold text-[11px] text-[rgba(51,51,51,0.5)]">
+                TUE
+              </div>
+              <div className="w-[36px] h-[36px] rounded-full bg-white text-[13px] font-alte font-bold flex justify-center items-center text-[rgba(51,51,51,0.5)]">
                 13
               </div>
-              <div className="text-[14px] font-wanted leading-[24px]">화</div>
             </div>
-            <div className="flex flex-col gap-[3px] items-center">
-              <div className="w-[36px] h-[36px] rounded-full bg-[#E3F0FF] border-[#333333] border text-[13px] font-alte font-bold flex justify-center items-center">
+            <div className="flex flex-col gap-[5px] items-center">
+              <div className="font-alte font-bold text-[11px] text-[rgba(51,51,51,0.5)]">
+                WED
+              </div>
+              <div className="w-[36px] h-[36px] rounded-full bg-white text-[13px] font-alte font-bold flex justify-center items-center text-[rgba(51,51,51,0.5)]">
                 14
               </div>
-              <div className="text-[14px] font-wanted leading-[24px]">수</div>
             </div>
-            <div className="flex flex-col gap-[3px] items-center">
-              <div className="w-[36px] h-[36px] rounded-full bg-[#E3F0FF] border-[#333333] border text-[13px] font-alte font-bold flex justify-center items-center">
+            <div className="flex flex-col gap-[5px] items-center">
+              <div className="font-alte font-bold text-[11px] text-[#333333]">
+                THU
+              </div>
+              <div className="w-[36px] h-[36px] rounded-full bg-white text-[13px] font-alte font-bold flex justify-center items-center text-[#333333]">
                 15
               </div>
-              <div className="text-[14px] font-wanted leading-[24px]">목</div>
             </div>
-            <div className="flex flex-col gap-[3px] items-center">
-              <div className="w-[36px] h-[36px] rounded-full bg-[#E3F0FF] border-[#333333] border text-[13px] font-alte font-bold flex justify-center items-center">
+            <div className="flex flex-col gap-[5px] items-center">
+              <div className="font-alte font-bold text-[11px] text-[rgba(51,51,51,0.5)]">
+                FRI
+              </div>
+              <div className="w-[36px] h-[36px] rounded-full bg-white text-[13px] font-alte font-bold flex justify-center items-center text-[rgba(51,51,51,0.5)]">
                 16
               </div>
-              <div className="text-[14px] font-wanted leading-[24px]">금</div>
             </div>
-            <div className="flex flex-col gap-[3px] items-center">
-              <div className="w-[36px] h-[36px] rounded-full bg-[#E3F0FF] border-[#333333] border text-[13px] font-alte font-bold flex justify-center items-center">
+            <div className="flex flex-col gap-[5px] items-center">
+              <div className="font-alte font-bold text-[11px] text-[rgba(51,51,51,0.5)]">
+                SAT
+              </div>
+              <div className="w-[36px] h-[36px] rounded-full bg-white text-[13px] font-alte font-bold flex justify-center items-center text-[rgba(51,51,51,0.5)]">
                 17
               </div>
-              <div className="text-[14px] font-wanted leading-[24px]">토</div>
             </div>
           </div>
         </div>
-        <div className="flex-1 w-full min-h-[208px]">
-          <div className="pt-[22px] ml-[7px] font-bold text-[20px] font-wanted leading-[22px]">
+        <div className="w-full h-[204px] bg-[#F8FFF6] pl-[24px]">
+          <div className="mt-[22px] font-wanted font-bold text-[16px] leading-[22px]">
             기도제목
           </div>
-          <div className="mt-[39px] px-[7px] flex justify-between h-[48px] items-center gap-[12px]">
-            <div>{"<"}</div>
-            {/* <div
-              className="flex-grow max-w-[296px] font-wanted text-[14px] leading-[24px] text-center break-words"
-              style={{
-                wordBreak: "keep-all",
-                overflowWrap: "break-word",
-              }}
-            >
-              야곱이 버드나무와 살구나무와 신풍나무의 푸른 가지를 그것들의
-              껍질을 벗겨 흰 무늬를 내기를 기도합니다.
-            </div> */}
-            <div className="flex-grow text-center">
-              <div
-                className="max-w-[296px] text-[14px] leading-[24px] font-wanted text-center break-words"
-                style={{
-                  wordBreak: "keep-all",
-                  overflowWrap: "break-word",
-                  display: "inline-block", // 핵심
-                }}
-              >
-                야곱이 버드나무와 살구나무와 신풍나무의 푸른 가지를 그것들의
-                껍질을 벗겨 흰 무늬를 내기를 기도합니다.
-              </div>
-            </div>
-            <div>{">"}</div>
+          <div
+            className="mt-[39px] pl-[28px] pr-[54px] font-wanted text-[14px] leading-[24px] text-center"
+            style={{
+              wordBreak: "keep-all",
+              overflowWrap: "break-word",
+            }}
+          >
+            야곱이 버드나무와 살구나무와 신풍나무의 푸른 가지를 그것들의 껍질을
+            벗겨 흰 무늬를 내기를 기도합니다.
           </div>
         </div>
       </div>
