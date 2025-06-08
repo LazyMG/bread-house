bcf748cc06ee987a6d2079eaa6f6e14aa97f959b
LazyMG <cbfmark@gmail.com>
2025-06-08 18:16:39 +0900

    style: 랜딩페이지 반응형 웹 양식 제목 정렬 수정 및 달력 콘솔 수정

--- components/home-bread-section.tsx
+++ components/home-bread-section.tsx
@@ -87,7 +87,7 @@ export default function BreadSection() {
           id="bread-section__title-info"
           className="mt-[4px] flex items-start pr-[31px] sm:pr-0 sm:relative sm:w-full sm:justify-center"
         >
-          <div className="font-wanted font-bold text-[24px] leading-[40px] w-[199px] break-words sm:font-extrabold lg:text-[42px] sm:w-[658px] sm:text-[30px] md:text-[36px] text-center">
+          <div className="font-wanted font-bold text-[24px] leading-[32px] w-[199px] break-words sm:font-extrabold lg:text-[42px] sm:w-[658px] sm:text-[30px] md:text-[36px] md:text-center">
             야곱의 노력과 승리는 어디로부터 나오나
           </div>
           <div className="ml-auto mt-[6px] text-[12px] leading-[16px] font-wanted text-left h-[32px] flex-shrink-0 sm:text-[16px] sm:absolute sm:right-[4%]">
