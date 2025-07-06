11f61d03d457eeaa387c238b71012168e16cc2e3
LazyMG <cbfmark@gmail.com>
2025-07-06 15:29:38 +0900

    style: /breads/:id 페이지 스크롤 테스트 #4

--- components/bread/verse-container.tsx
+++ components/bread/verse-container.tsx
@@ -23,7 +23,7 @@ const VerseContainer = ({
     <>
       <div
         id="left-section__content"
-        className="mt-[16px] w-full h-fit sm:h-[480px] flex flex-col gap-[12px] sm:scrollbar-hide sm:overflow-y-scroll cursor-pointer mb-[200px] sm:mb-0"
+        className="mt-[16px] w-full sm:h-[480px] flex flex-col gap-[12px] sm:scrollbar-hide sm:overflow-y-scroll cursor-pointer mb-[50px] sm:mb-0"
       >
         {verses.map((verse, index) => (
           <div
