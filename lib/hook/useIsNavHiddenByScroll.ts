66e4a496f748a7e5b1428cdc281eb118cb7e1343
LazyMG <cbfmark@gmail.com>
2025-07-08 22:29:58 +0900

    style: 모바일 오양페이지 스크롤 이벤트 성능 테스트 이벤트 훅에 requestAnimationFrame 추가

--- lib/hook/useIsNavHiddenByScroll.ts
+++ lib/hook/useIsNavHiddenByScroll.ts
@@ -8,43 +8,77 @@ export const useIsNavHiddenByScroll = () => {
   );
   const lastScrollY = useRef(0);
 
+  // useEffect(() => {
+  //   const isMobile = window.innerWidth < 768;
+  //   if (!isMobile) {
+  //     setIsNavHiddenByScroll(false);
+  //     return;
+  //   }
+
+  //   const threshold = 6;
+
+  //   const handleScroll = () => {
+  //     const currentScrollY = window.scrollY;
+  //     const delta = currentScrollY - lastScrollY.current;
+
+  //     const navHeight = 72;
+
+  //     // 아래로 확실히 스크롤 했을 때만 숨김
+  //     if (delta > threshold && currentScrollY > navHeight) {
+  //       setIsNavHiddenByScroll(true);
+  //     }
+  //     // 위로 확실히 스크롤 했을 때만 표시
+  //     else if (delta < -threshold) {
+  //       setIsNavHiddenByScroll(false);
+  //     }
+
+  //     // 맨 위에 있으면 무조건 표시
+  //     if (currentScrollY === 0) {
+  //       setIsNavHiddenByScroll(false);
+  //     }
+
+  //     lastScrollY.current = currentScrollY;
+  //   };
+
+  //   window.addEventListener("scroll", handleScroll);
+
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, []);
+
   useEffect(() => {
     const isMobile = window.innerWidth < 768;
-    if (!isMobile) {
-      setIsNavHiddenByScroll(false);
-      return;
-    }
+    if (!isMobile) return;
 
     const threshold = 6;
+    let ticking = false;
 
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
       const delta = currentScrollY - lastScrollY.current;
 
-      const navHeight = 72;
+      const update = () => {
+        const navHeight = 50;
 
-      // 아래로 확실히 스크롤 했을 때만 숨김
-      if (delta > threshold && currentScrollY > navHeight) {
-        setIsNavHiddenByScroll(true);
-      }
-      // 위로 확실히 스크롤 했을 때만 표시
-      else if (delta < -threshold) {
-        setIsNavHiddenByScroll(false);
-      }
+        if (delta > threshold && currentScrollY > navHeight) {
+          setIsNavHiddenByScroll(true);
+        } else if (delta < -threshold || currentScrollY === 0) {
+          setIsNavHiddenByScroll(false);
+        }
 
-      // 맨 위에 있으면 무조건 표시
-      if (currentScrollY === 0) {
-        setIsNavHiddenByScroll(false);
-      }
+        lastScrollY.current = currentScrollY;
+        ticking = false;
+      };
 
-      lastScrollY.current = currentScrollY;
+      if (!ticking) {
+        requestAnimationFrame(update);
+        ticking = true;
+      }
     };
 
     window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return isNavHiddenByScroll;
