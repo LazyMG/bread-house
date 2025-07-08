089b5a54d629363a0f604d334988163c0c702247
LazyMG <cbfmark@gmail.com>
2025-07-08 15:18:23 +0900

    refactor: 주석 제거

--- components/navigation/navigation.tsx
+++ components/navigation/navigation.tsx
@@ -14,42 +14,6 @@ const Navigation = () => {
   );
   const lastScrollY = useRef(0); // 이전 스크롤 위치를 저장
 
-  // useEffect(() => {
-  //   if (isNavOpen) return;
-
-  //   const isMobile = window.innerWidth < 768;
-
-  //   if (!isMobile) {
-  //     setIsNavHiddenByScroll(false);
-  //     return;
-  //   }
-
-  //   const handleScroll = () => {
-  //     const currentScrollY = window.scrollY;
-  //     const navHeight = 72;
-
-  //     if (currentScrollY > lastScrollY.current && currentScrollY > navHeight) {
-  //       setIsNavHiddenByScroll(true);
-  //     } else if (currentScrollY < lastScrollY.current) {
-  //       setIsNavHiddenByScroll(false);
-  //     }
-
-  //     if (currentScrollY <= navHeight && lastScrollY.current > navHeight) {
-  //       setIsNavHiddenByScroll(false);
-  //     } else if (currentScrollY === 0) {
-  //       setIsNavHiddenByScroll(false);
-  //     }
-
-  //     lastScrollY.current = currentScrollY; // 현재 스크롤 위치 저장
-  //   };
-
-  //   window.addEventListener("scroll", handleScroll);
-
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, [isNavOpen]);
-
   useEffect(() => {
     if (isNavOpen) return;
 
