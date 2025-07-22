3c92659d41e71c0a5f88a304dbf0be3209990c8b
LazyMG <cbfmark@gmail.com>
2025-07-22 14:39:07 +0900

    feat: 카카오 로그인 테스트 중 에러 발생 시 리다이렉트

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -11,26 +11,30 @@ export async function GET(request: Request) {
     console.log("code", code);
     // 서버에 요청 보내기
 
-    // 성공
-    const response = await fetch(
-      "http://localhost:8080/quiet-time/api/auth/kakao",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json", // 요청 본문이 JSON임을 명시
-        },
-        // 요청 본문 (body)에는 인가 코드를 JSON 형태로 담아 보냅니다.
-        // Spring Boot에서는 이 JSON을 받을 DTO(Data Transfer Object)를 정의해야 합니다.
-        body: JSON.stringify({ code }),
-      }
-    );
-    console.log(response);
-    console.log("send!");
+    try {
+      // 성공
+      const response = await fetch(
+        "http://localhost:8080/quiet-time/api/auth/kakao",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json", // 요청 본문이 JSON임을 명시
+          },
+          // 요청 본문 (body)에는 인가 코드를 JSON 형태로 담아 보냅니다.
+          // Spring Boot에서는 이 JSON을 받을 DTO(Data Transfer Object)를 정의해야 합니다.
+          body: JSON.stringify({ code }),
+        }
+      );
+      console.log(response);
+      console.log("send!");
 
-    // 실패
-    // 실패면 다른 페이지로 돌리기
-    if (!response.ok) {
-      alert("서버 통신 실패");
+      // 실패
+      // 실패면 다른 페이지로 돌리기
+      if (!response.ok) {
+        return NextResponse.redirect(`${origin}${next}`);
+      }
+    } catch (error) {
+      console.log("error", error);
       return NextResponse.redirect(`${origin}${next}`);
     }
   } else {
